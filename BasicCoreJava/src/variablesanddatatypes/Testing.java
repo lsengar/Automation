3a00550b449ec12c4dd450f47625174c61052ea8
lsengar <lokpratapsingh@gmail.com>
2019-03-02 15:45:21 +0530

    Adding 2 scripts

---
+++ BasicCoreJava/src/variablesanddatatypes/Testing.java
@@ -0,0 +1,12 @@
+package variablesanddatatypes;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("jkjdsfewrrrrrrr");
+
+	}
+
+}
